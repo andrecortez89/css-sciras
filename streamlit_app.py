5e64f1e1d985bfaacc2aecbbc645e9775eebc3f2
andrecortez89 <andrecortez89@gmail.com>
2024-06-01 04:33:55 +0000

    ultimate

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,50 +1,32 @@
 import streamlit as st
-import numpy as np
 import pandas as pd
-import altair as alt
 
-# Page title
-st.set_page_config(page_title='Interactive Data Explorer', page_icon='📊')
-st.title('📊 Interactive Data Explorer')
+def load_data():
+    # Load the CSV file
+    # Adjust the file path if your environment requires it
+    return pd.read_csv('data/profilaxia.csv')
 
-with st.expander('About this app'):
-  st.markdown('**What can this app do?**')
-  st.info('This app shows the use of Pandas for data wrangling, Altair for chart creation and editable dataframe for data interaction.')
-  st.markdown('**How to use the app?**')
-  st.warning('To engage with the app, 1. Select genres of your interest in the drop-down selection box and then 2. Select the year duration from the slider widget. As a result, this should generate an updated editable DataFrame and line plot.')
-  
-st.subheader('Which Movie Genre performs ($) best at the box office?')
+def main():
+    st.title('Profilaxia Cirúrgica SCIRAS HCSS & PG')
+    
+    # Load data
+    df = load_data()
 
-# Load data
-df = pd.read_csv('data/movies_genres_summary.csv')
-df.year = df.year.astype('int')
+    # Optionally display the raw data
 
-# Input widgets
-## Genres selection
-genres_list = df.genre.unique()
-genres_selection = st.multiselect('Select genres', genres_list, ['Action', 'Adventure', 'Biography', 'Comedy', 'Drama', 'Horror'])
+    # Filtering options directly in the main page
+    st.subheader('Selecionar por Especialidade')
+    
+    # Get unique values from the 'Especialidade' column for filtering
+    especialidades = df['Especialidade'].unique()
+    especialidade_selection = st.multiselect('Especialidade', especialidades)
 
-## Year selection
-year_list = df.year.unique()
-year_selection = st.slider('Select year duration', 1986, 2006, (2000, 2016))
-year_selection_list = list(np.arange(year_selection[0], year_selection[1]+1))
+    # Display filtered data
+    if especialidade_selection:
+        filtered_data = df[df['Especialidade'].isin(especialidade_selection)]
+        st.dataframe(filtered_data)  # Using st.dataframe for better formatting
+    else:
+        st.write("Selecione a Especialidade para visualizar.")
 
-df_selection = df[df.genre.isin(genres_selection) & df['year'].isin(year_selection_list)]
-reshaped_df = df_selection.pivot_table(index='year', columns='genre', values='gross', aggfunc='sum', fill_value=0)
-reshaped_df = reshaped_df.sort_values(by='year', ascending=False)
-
-
-# Display DataFrame
-
-df_editor = st.data_editor(reshaped_df, height=212, use_container_width=True,
-                            column_config={"year": st.column_config.TextColumn("Year")},
-                            num_rows="dynamic")
-df_chart = pd.melt(df_editor.reset_index(), id_vars='year', var_name='genre', value_name='gross')
-
-# Display chart
-chart = alt.Chart(df_chart).mark_line().encode(
-            x=alt.X('year:N', title='Year'),
-            y=alt.Y('gross:Q', title='Gross earnings ($)'),
-            color='genre:N'
-            ).properties(height=320)
-st.altair_chart(chart, use_container_width=True)
+if __name__ == '__main__':
+    main()
